6c08168e17b5eaae661186fa9d12d68c951adb7e
Jonathan Kim <jonkim204@gmail.com>
2018-06-20 09:07:53 -0700

    createItem method

--- scripts/api.js
+++ scripts/api.js
@@ -6,5 +6,17 @@ const api = (function() {
   const getItems = function(callback) {
     $.getJSON(`${BASE_URL}/items`, callback);
   };
-  return {getItems};
+  const createItem = function(name, callback) {
+    const newItem = JSON.stringify({name});
+    $.ajax({
+      url: `${BASE_URL}/items`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: newItem,
+      success: callback
+    });
+  };
+  
+
+  return {getItems, createItem};
 }());
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,5 @@ $(document).ready(function() {
 });
 
 store.items.push(Item.create('apples'));
+
+
